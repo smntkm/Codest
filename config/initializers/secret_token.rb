078604c94950a87d7792f36c3909d18bd1446bd1
青山 貴海 <aoyamatakami@device-8bcdc5.(none)>
2013-10-15 14:35:28 +0900

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Codest::Application.config.secret_key_base = 'b0f38861db2bd7844bd943b04b83c5f61922115d8052068c20be41689924a8ef836c02b182a90f1879109a8a691ec3262f83497f1cdb47b2b43e30d4f2070370'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Codest::Application.config.session_store :cookie_store, key: '_Codest_session'
