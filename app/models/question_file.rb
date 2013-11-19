261738466bbfde03eee6492ca019d2f4011e8ba4
青山 貴海 <xxx.backspace.0000@gmail.com>
2013-11-19 18:14:22 +0900

    test変更

--- app/models/question_file.rb
+++ app/models/question_file.rb
@@ -1,8 +1,8 @@
 class QuestionFile < ActiveRecord::Base
   validates :name, :content_type, :data, presence: true
   
-  belongs_to :question, foreign_key: "file_id"
-  belongs_to :answer, foreign_key: "file_id"
+  belongs_to :question, foreign_key: "id"
+  belongs_to :answer, class_name: "Answer" 
 
   def send_file file
    self.name = file.original_filename
