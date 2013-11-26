ef3d73bf3fd76e5b781a0f8f8a5fd4bbdc2cbb5f
青山 貴海 <xxx.backspace.0000@gmail.com>
2013-11-26 12:17:08 +0900

    questionのfile_idの名前の変更

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131126022430) do
+ActiveRecord::Schema.define(version: 20131126031240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,11 +26,11 @@ ActiveRecord::Schema.define(version: 20131126022430) do
   end
 
   create_table "questions", force: true do |t|
-    t.string   "title",      null: false
-    t.text     "content",    null: false
+    t.string   "title",        null: false
+    t.text     "content",      null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "file_id"
+    t.integer  "user_file_id"
   end
 
   create_table "user_files", force: true do |t|
