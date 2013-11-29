c20dcaf1caa38b0faa85b373350865ca0932231a
青山 貴海 <xxx.backspace.0000@gmail.com>
2013-11-29 11:47:48 +0900

    reports_controllerのテスト追加

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe ReportsController do
+  describe "POST create" do
+    specify "レポート表示ページが表示される" do
+      answer = FactoryGirl.create(:answer)
+      post :create, { answer: answer }
+
+      response.should render_template("report")
+    end
+  end
 
 end
