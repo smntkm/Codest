cbe5e2bb2c85b9008ba98e2839648bc938b140d2
青山 貴海 <xxx.backspace.0000@gmail.com>
2013-12-03 16:17:59 +0900

    レポート作成

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,26 @@
 class ReportsController < ApplicationController
   def create
+    file_create
     render "report"
+  end
+
+  private
+  def file_create
+    answer = Answer.find(params[:id])
+    question = Question.find(answer.question_id)
+    
+    t = Time.now.strftime("%Y%m%d%H%M%S%L") 
+    exe_file = Tempfile.new(["#{t}", ".rb"], "./tmp")
+
+    #exe_file.write(answer.user_file.data)
+    #exe_file.write(question.user_file.data)
+    exe_file.write("class Sample\n  def cat str1, str2\n    \"\#{str1}\#{str2}\"\n  end\nend\nrequire \"test/unit\"\n\nclass Sample_test < Test::Unit::TestCase\n  def test_aa_bb\n    s = Sample.new\n    actual = s.cat(\"aa\", \"bb\")\n    assert_equal(\"aabb\", actual)\n  end\n\n  def test_aa_11\n    s = Sample.new\n    actual = s.cat(\"aa\", \"bb\")\n    assert_equal(\"aa11\", actual)\n  end\nend\n")
+    exe_file.rewind
+
+    test_exe exe_file
+  end
+
+  def test_exe exe_file
+    @result = Open3.capture3("ruby #{File.path(exe_file)}")
   end
 end
--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -8,6 +8,38 @@ describe ReportsController do
 
       response.should render_template("report")
     end
+
+    specify "テスト実行結果の作成" do
+      #問題作成
+      s = Dir::pwd.to_s + ("/spec/factories/sample_test.rb")
+      f = Rack::Test::UploadedFile.new(s, "text/x-ruby-script")
+      question_file = UserFile.new
+      question_file.send_file(f)
+      q = Question.new
+      q.user_file = question_file
+      q.title = "問題1"
+      q.content = "2つの文字列を連結し、返すテストの作成"
+      q.save
+    
+      #解答の作成
+      as = Dir::pwd.to_s + ("/spec/factories/sample.rb")
+      af = Rack::Test::UploadedFile.new(as, "text/x-ruby-script")
+      answer_file = UserFile.new
+      answer_file.send_file(af)
+      a = Answer.new
+      a.user_file = answer_file
+      a.user_name = "Mike"
+      a.question_id = q.id
+      a.email = "hoge@sample.com"
+      a.save
+
+      post :create, { id: a }
+      result = assigns(:result)
+
+      expect(result.size).to eq 3
+      expect(result[0]).to be_true
+      expect(result[1]).to eq ""
+    end
   end
 
 end
