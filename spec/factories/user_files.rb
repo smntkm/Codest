8363db9a9f399eb3f131e9a78f6653bfb196a61a
青山 貴海 <xxx.backspace.0000@gmail.com>
2013-12-03 16:19:09 +0900

    テスト修正

--- spec/factories/user_files.rb
+++ spec/factories/user_files.rb
@@ -1,12 +1,12 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  s = Dir::pwd.to_s + ("/spec/factories/test.rb")
+  s = Dir::pwd.to_s + ("/spec/factories/sample.rb")
   f = Rack::Test::UploadedFile.new(s, "text/x-ruby-script")
 
   factory :user_file do
-    self.name f.original_filename    #test.rb
+    self.name f.original_filename    #sample.rb
     self.content_type f.content_type #text/x-ruby-script
-    self.data f.read                 #test = "test"\n
+    self.data f.read                 #内容
   end
 end
