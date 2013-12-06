86f2150bd188cab021914afea84c79e7b3b5ced3
青山 貴海 <xxx.backspace.0000@gmail.com>
2013-12-06 15:42:42 +0900

    topへのルーティング指定

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Codest::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'questions#index'
+  root 'top#top'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
