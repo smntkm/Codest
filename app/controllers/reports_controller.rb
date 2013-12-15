943d667e90a4b1ad7cf78acf33bb8a57b095b89c
青山 貴海 <xxx.backspace.0000@gmail.com>
2013-12-15 22:10:39 +0900

    レポートコントローラの修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -9,12 +9,16 @@ class ReportsController < ApplicationController
     answer = Answer.find(params[:id])
     question = Question.find(answer.question_id)
     
-    t = Time.now.strftime("%Y%m%d%H%M%S%L") 
+    #一意性を上げるためのタイムスタンプ
+    t = Time.now.strftime("%Y%m%d%H%M%S%L")
+    #一意のテンプファイルの作成
     exe_file = Tempfile.new(["#{t}", ".rb"], "./tmp")
 
-    #exe_file.write(answer.user_file.data)
-    #exe_file.write(question.user_file.data)
-    exe_file.write("class Sample\n  def cat str1, str2\n    \"\#{str1}\#{str2}\"\n  end\nend\nrequire \"test/unit\"\n\nclass Sample_test < Test::Unit::TestCase\n  def test_aa_bb\n    s = Sample.new\n    actual = s.cat(\"aa\", \"bb\")\n    assert_equal(\"aabb\", actual)\n  end\n\n  def test_aa_11\n    s = Sample.new\n    actual = s.cat(\"aa\", \"bb\")\n    assert_equal(\"aa11\", actual)\n  end\nend\n")
+    #それぞれのファイルの書き込み
+    exe_file.write(answer.user_file.data)
+    exe_file.write(question.user_file.data)
+    
+    #ファイルポインタを先頭にする
     exe_file.rewind
 
     test_exe exe_file
