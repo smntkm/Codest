f536f37e6204388ed56413a13100cefc1d3fddad
青山 貴海 <xxx.backspace.0000@gmail.com>
2013-12-17 14:39:09 +0900

    ルートの設定の修正

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Codest::Application.routes.draw do
   # レポート作成
   match "/reports/:id" => "reports#create", via: :get, as: "report"
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
