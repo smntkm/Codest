854f93bfaa22221557f7d3041320d1c8da120d6a
青山 貴海 <xxx.backspace.0000@gmail.com>
2013-12-24 14:07:28 +0900

    add column email password to questions

--- db/migrate/20131224044251_add_email_and_password_to_questions.rb
+++ db/migrate/20131224044251_add_email_and_password_to_questions.rb
@@ -1,4 +1,11 @@
 class AddEmailAndPasswordToQuestions < ActiveRecord::Migration
-  def change
+  def up
+    add_column :questions, :email, :String
+    add_column :questions, :password, :String 
+  end
+
+  def down
+    remove_column :questions, :email
+    remove_column :questions, :password
   end
 end
