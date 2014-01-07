1a514a13545d6051c0a5239804fc99a49e93aad4
青山 貴海 <xxx.backspace.0000@gmail.com>
2014-01-07 14:36:41 +0900

    出題者が問題削除用のルーティング作成のテスト追加

--- spec/routing/setting/questions_routing_spec.rb
+++ spec/routing/setting/questions_routing_spec.rb
@@ -30,5 +30,9 @@ describe Setting::QuestionsController do
     it "routes to #destroy" do
       delete("/setting/questions/1").should route_to("setting/questions#destroy", :id => "1")
     end
+
+    specify "routes to #same_password" do
+      post("questions/1/same_password").should route_to("questions#same_password", id: "1")
+    end
   end
 end
