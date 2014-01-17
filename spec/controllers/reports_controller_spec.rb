331c78dd84816f0477359bcafdf7ae803b767640
青山 貴海 <xxx.backspace.0000@gmail.com>
2014-01-17 13:27:46 +0900

    reportの作成テストの変更

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -1,41 +1,32 @@
 require 'spec_helper'
 
 describe ReportsController do
+  let(:question) { FactoryGirl.create(:question) }
+  let(:data) do
+    as = Dir::pwd.to_s + ("/spec/factories/sample.rb")
+    Rack::Test::UploadedFile.new(as, "text/x-ruby-script")
+  end
+  let(:valid_attributes) { { question_id: question.id, data: data } }
+
+
+  describe "Get new" do
+    specify "フォーム画面に移動出来る" do
+      get :new
+
+      expect(response).to render_template("new")
+    end
+  end
+
   describe "POST create" do
     specify "レポート表示ページが表示される" do
-      answer = FactoryGirl.create(:answer)
-      post :create, { id: answer }
+      question = FactoryGirl.create(:question)
+      post :create, valid_attributes
 
       response.should render_template("report")
     end
 
     specify "テスト実行結果の作成" do
-      #問題作成
-      s = Dir::pwd.to_s + ("/spec/factories/sample_test.rb")
-      f = Rack::Test::UploadedFile.new(s, "text/x-ruby-script")
-      question_file = UserFile.new
-      question_file.send_file(f)
-      q = Question.new
-      q.user_file = question_file
-      q.title = "問題1"
-      q.content = "2つの文字列を連結し、返すテストの作成"
-      q.email = "hoge@hoge.com"
-      q.password = "password"
-      q.save
-    
-      #解答の作成
-      as = Dir::pwd.to_s + ("/spec/factories/sample.rb")
-      af = Rack::Test::UploadedFile.new(as, "text/x-ruby-script")
-      answer_file = UserFile.new
-      answer_file.send_file(af)
-      a = Answer.new
-      a.user_file = answer_file
-      a.user_name = "Mike"
-      a.question_id = q.id
-      a.email = "hoge@sample.com"
-      a.save
-
-      get :create, { id: a }
+      post :create, valid_attributes
       result = assigns(:result)
 
       expect(result.size).to eq 3
@@ -43,30 +34,8 @@ describe ReportsController do
       expect(result[1]).to eq ""
     end
 
-    specify "解答がDBから削除されている" do
-      question_file = FactoryGirl.create(:question_file)
-      question = FactoryGirl.create(:question, user_file: question_file)
-      question.save
-      answer_file = FactoryGirl.create(:answer_file)
-      answer = FactoryGirl.create(:answer, user_file: answer_file)
-      answer.question = question
-      answer.save
-
-      expect {
-        get :create, { id: answer }
-      }.to change(Answer, :count).by -1
-    end
-
     specify "英語の部分がが日本語に成っている" do
-      question_file = FactoryGirl.create(:question_file)
-      question = FactoryGirl.create(:question, user_file: question_file)
-      question.save
-      answer_file = FactoryGirl.create(:answer_file)
-      answer = FactoryGirl.create(:answer, user_file: answer_file)
-      answer.question = question
-      answer.save
-
-      get :create, { id: answer }
+      post :create, valid_attributes
 
       result =  assigns(:result)
       expect(result[0]).to include "失敗"
