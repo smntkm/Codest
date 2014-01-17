41881167d1a8fc8d9e7fe0661bc4ea772c476ce9
青山 貴海 <xxx.backspace.0000@gmail.com>
2014-01-17 13:29:01 +0900

    report作成の実装変更

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,14 +1,16 @@
 class ReportsController < ApplicationController
+  def new
+  end
+
   def create
-    file_create
-    answer_delete
+    report_create
     render "report"
   end
 
   private
-  def file_create
-    answer = Answer.find(params[:id])
-    question = Question.find(answer.question_id)
+  def report_create
+    reportfile = params[:data]
+    question = Question.find(params[:question_id])
     
     #一意性を上げるためのタイムスタンプ
     time = Time.now.strftime("%Y%m%d%H%M%S%L")
@@ -16,16 +18,13 @@ class ReportsController < ApplicationController
     exe_file = Tempfile.new(["#{time}", ".rb"], "./tmp")
 
     #それぞれのファイルの書き込み
-    File.open(exe_file, "ab") {|f| f.write(answer.user_file.data) }
+    File.open(exe_file, "ab") {|f| f.write(reportfile.read) }
     File.open(exe_file, "ab") {|f| f.write(question.user_file.data)  }
     
-    #ファイルポインタを先頭にする
-    exe_file.rewind
-
-    test_exe exe_file
+    exe_test exe_file
   end
 
-  def test_exe exe_file
+  def exe_test exe_file
     @result = Open3.capture3("ruby #{File.path(exe_file)}")
     result_format
   end
@@ -38,9 +37,5 @@ class ReportsController < ApplicationController
     @result[0].gsub!(/failures/, "失敗")
     @result[0].gsub!(/errors/, "エラー")
     @result[0].gsub!(/skips/, "スキップ")
-  end
-
-  def answer_delete
-    Answer.destroy(params[:id])
   end
 end
